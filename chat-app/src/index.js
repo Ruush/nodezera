7582e91cb4681e376858bda1af54c64543650fe5
Ruush <gabriel.alvesferreira@gmail.com>
2019-10-08 17:00:01 -0300

    aula 171

---
+++ chat-app/src/index.js
@@ -0,0 +1,73 @@
+const path = require('path')
+const http = require('http')
+
+const express = require('express')
+const socketio = require('socket.io')
+const Piii = require('piii')
+const piiiFilters = require('piii-filters')
+
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { getUser, getUsersInRoom, removeUser, addUser } = require('./utils/users')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection.')
+
+    socket.on('join', (options, callback) => {
+        const { error, user } = addUser({
+            id: socket.id,
+            username: options.username,
+            room: options.room
+        })
+        if (error) {
+            return callback(error)
+        }
+
+        socket.join(user.room)
+
+        socket.emit('message', generateMessage("Admin", "Welcome!"))
+        socket.broadcast.to(user.room).emit('message', generateMessage("Admin", `${user.username} has joined! :D`))
+
+        callback()
+    })
+
+    socket.on('sendMessage', (message, callback) => {
+        const { username, room } = getUser(socket.id)
+        const piii = new Piii({ filters: [...Object.values(piiiFilters)] })
+
+        if (piii.has(message)) {
+            return callback('Palavrao n pode pacero')
+        }
+
+        io.to(room).emit('message', generateMessage(username, message))
+        callback()
+    })
+
+
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id)
+
+        if (user) {
+            io.to(user.room).emit('message', generateMessage("Admin", `${user.username} has left! :(`))
+        }
+    })
+
+    socket.on('sendLocation', ({ latitude, longitude }, callback) => {
+        const { username, room } = getUser(socket.id)
+        io.to(room).emit('locationMessage', generateLocationMessage(username, `https://google.com/maps?q=${latitude},${longitude}`))
+        callback()
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server up on port ${port}`)
+})
--- task-manager/src/emails/account.js
+++ task-manager/src/emails/account.js
@@ -4,7 +4,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'gabriel@acosvic.com.br',
+        from: 'gabrielalves5582@gmail.com',
         subject: 'Thanks for joining in!',
         text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
     })
@@ -13,11 +13,8 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancelationEmail = (email, name) => {
     sgMail.send({
         to: email,
-        //from: 'gayuganda@gmail.com',
-        from: 'gabriel@acosvic.com.br',
-        //subject: 'Why are u gae?',
+        from: 'gabrielalves5582@gmail.com',
         subject: 'Sorry to see you go!',
-        //html: `<h1>you are gue</h1><br><img src="https://images-cdn.9gag.com/photo/aqKE98Z_700b.jpg">`
         text: `Goodbye, ${name}. I hope to see you back sometime soon.`
     })
 }
